4ac94281707b9a20ad1f5aa3b86ae5776d7cb04d
Lorezz <point.point@gmail.com>
2018-07-11 18:08:41 +0200

    added editor webapp sfaccold

---
+++ editor/src/app/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
+import Layout from "./components/_layout";
+import Index from "./components/index";
+
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Layout>
+          <Index />
+        </Layout>
+      </Provider>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
---
+++ editor/src/app/components/_layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "../../asset/style.scss";
+
+import ReactNotify from "react-notify";
+import { connect } from "react-redux";
+import Header from "./_header";
+import Footer from "./_footer";
+
+@connect(state => {
+  return {
+    notifications: state.notifications
+  };
+})
+export default class Layout extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (
+      nextProps.notifications &&
+      nextProps.notifications != this.props.notifications &&
+      nextProps.notifications.item
+    ) {
+      let n = nextProps.notifications.item;
+      let { title, msg, millis } = n;
+      this.refs.notificator.success(title, msg, millis);
+    }
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <ReactNotify ref="notificator" />
+        {false && <Header />}
+        <div className="main">{this.props.children}</div>
+        <Footer />
+      </div>
+    );
+  }
+}
